f33c3e3f61efd4df81c38c48b605308f35f91ad1
sofiia <sofiia.khapal@nure.ua>
2026-02-04 20:50:36 +0100

    solution

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -14,7 +14,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
-    @OneToMany(cascade = CascadeType.PERSIST    )
+    @OneToMany(cascade = CascadeType.PERSIST)
     private List<Comment> comments;
 
     public Long getId() {
